3d3f004251dd8907171eee1d20f7709b14c1adcc
Festus Kiprotich <m1ndnyong1@gmail.com>
2024-04-09 14:01:04 +0300

    This is a TPOK Jazz playlist

---
+++ main.py
@@ -0,0 +1,13 @@
+songs = "Mamou, Maby, Africa, Eau Bénite, Mere Superieure, Zadio, OK Madame, Beyanga, Lisanga Ya Bambanda, Pauline, Ebale Ya Zaire, Francine, Bina na Ngai na Respect, Afida Na Ngai, Sorozo, Francine, Monie, Mfumbe, Liyanzi Ekoti Ngai Na Motema, Moziki, Nalembi Nalembi, Libala Ya Madeleine, Pesa Position, Editha, Echauffement 1 & 2, Cielo, Merengue, Yeba Buana, Mimi Et Mimi, Pamphile, Marie-Josée, Kamikaze, Lina, Fongola, La Reine De Sabah, Ida, Nati Congo, Canta Moçambique, Dembela, BB 69, N'Zoto Souvenir, Mokili, Mwana Wabi, On Entre OK, On Sort KO, Mamu, Nakomitunaka, Sala Keba, Doris Iloko, Liwa Ya Somo, Chouchouna."
+playlist = songs.split(", ")
+print(playlist)
+
+playlist_size = len(playlist)
+print(playlist_size)
+
+artist = "tpok jazz"
+print(artist.title())
+
+currently_playing = songs[0].title()
+print(currently_playing)
+
